e6d3dab142d02be2e397856e8ac3190d3988a3cb
Avtandil Shaburishvili <a.shaburishvili@gmail.com>
2021-10-14 10:13:35 +0400

    about us

--- src/Containers/AboutUspage/AboutUsPage.js
+++ src/Containers/AboutUspage/AboutUsPage.js
@@ -7,23 +7,20 @@ import Layout from './../../Components/Layout/Layout';
 const AboutUsPage = () => {
     return (
         <Layout>
-             <div className = 'au-cont' >
-                
-                <div className = 'au-cont-right'>
-                    <div className = 'text'>
-                    <p>P&amp;S Group დაკომპლექტებულია ოპერატიული და საგამოძიებო საქმიანობის სფეროში მრავალწლიანი გამოცდილების მქონე კადრებით.</p>
-                    </div>
-                    <div className = 'text'>
-                    <p>ჩვენი გუნდი მზად არის იმუშაოს 24/7, რათა თქვენთვის სასურველი მომსახურება მიიღოთ სწრაფად და ხარისხიანად.</p>
-                    </div>
-                    <div className = 'text'>
-                    <p>ჩვენ ვმოქმედებთ კანონმდებლობის შესაბამისად და კონფიდენციალურობის სრული დაცვით.</p>
-                    </div>
-                </div>
-                <div className = 'au-cont-left'>
+                <div className='au-cont' >
 
-                </div>
-
+                    <div className='au-cont-right'>
+                        <div className='text-ab-us'>
+                            <p>P&amp;S Group დაკომპლექტებულია ოპერატიული და საგამოძიებო საქმიანობის სფეროში მრავალწლიანი გამოცდილების მქონე კადრებით.</p>
+                        </div>
+                        <div className='text-ab-us'>
+                            <p>ჩვენი გუნდი მზად არის იმუშაოს 24/7, რათა თქვენთვის სასურველი მომსახურება მიიღოთ სწრაფად და ხარისხიანად.</p>
+                        </div>
+                        <div className='text-ab-us'>
+                            <p>ჩვენ ვმოქმედებთ კანონმდებლობის შესაბამისად და კონფიდენციალურობის სრული დაცვით.</p>
+                        </div>
+                    </div>
+                    
             </div>
         </Layout>
     )
